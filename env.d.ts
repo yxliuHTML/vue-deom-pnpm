42836ead483e3cafa0f296230c7286928f6c4934
yxliu <79382207@qq.com>
2023-06-21 10:55:35 +0800

    feat:将el全局引入改为自动导入

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+import { MessageType } from 'element-plus/lib/el-message/src/types'
+
+declare global {
+  interface Window {
+    $toast: (type: MessageType, message: string) => Promise<void>
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
@@ -11,7 +9,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
